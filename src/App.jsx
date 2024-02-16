c21fdea8d340cb3a848b85eb8e384bebe05cbd6e
Aiman Ahmad <khairulaiman@Aimans-MacBook-Pro.local>
2024-02-16 09:56:06 +0800

    add resume section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Home from "./components/home/Home";
 import Skills from "./components/skills/Skills";
 import Portfolio from "./components/portfolio/Portfolio";
+import Resume from "./components/resume/Resume";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Home />
       <Skills />
       <Portfolio />
+      <Resume />
     </main>
   );
 }
